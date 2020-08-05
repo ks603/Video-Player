4022ca03a97ae7282c403250a77b4bbd6d0dec3e
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-05 18:21:16 -0500

    Wrote all the variable to target the HTML

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-
+const video = document.querySelector('video')
+const progressRange = document.querySelector('.progress-range')
+const progressBar = document.querySelector('.progress-bar')
+const palyBtn = document.getElementById('play-btn')
+const volumeIcon = document.getElementById('volume-icon')
+const volumeRange = document.querySelector('.volume-range')
+const volumeBar = document.querySelector('.volume-bar')
+const currentTime = document.getElementById('.time-elapsed')
+const duration = document.querySelector('.time-duration')
+const fullscreenBtn = document.querySelector('.fullscreen')
 
 // Play & Pause ----------------------------------- //
 
-
-
 // Progress Bar ---------------------------------- //
-
-
 
 // Volume Controls --------------------------- //
 
-
-
 // Change Playback Speed -------------------- //
 
-
-
 // Fullscreen ------------------------------- //
-
-
