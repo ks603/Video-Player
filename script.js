dc585a1e1258b9fe039df9320e6a9698701cc9b0
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-05 19:37:47 -0500

    Volume progress bar and volume now adjusts to where you click

--- script.js
+++ script.js
@@ -58,6 +58,21 @@ function setProgress(e) {
 
 // Volume Controls --------------------------- //
 
+// Volume Bar
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth
+  // Rounding volume up or down
+  if (volume < 0.1) {
+    volulme = 0
+  }
+  if (volume > 0.9) {
+    volume = 1
+  }
+  volumeBar.style.width = `${volume * 100}%`
+  video.volume = volume
+  console.log(e)
+}
+
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
@@ -68,3 +83,4 @@ video.addEventListener('click', togglePlay)
 video.addEventListener('timeupdate', updateProgress)
 video.addEventListener('canplay', updateProgress)
 progressRange.addEventListener('click', setProgress)
+volumeRange.addEventListener('click', changeVolume)
